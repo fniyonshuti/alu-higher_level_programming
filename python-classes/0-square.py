ae219d9cd368158c99434587e14d7f5a822e31a6
fniyonshuti <f.niyonshut1@alustudent.com>
2024-03-14 02:04:00 -0700

    an empty  class square defined

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Create an Empty Square """
+
+"""
+Create an Empty Square
+"""
 
 class Square:
-    ''' creates an empty square'''
-
+    """
+    Creates an empty square
+    """
     pass
 
 
