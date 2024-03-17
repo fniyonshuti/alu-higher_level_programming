8c9754378554f5ddf28acf147a48090d6c6ea697
fniyonshuti <f.niyonshut1@alustudent.com>
2024-03-17 10:45:19 -0700

    class square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Square class definition"""
+"""a class Square that defines a square"""
 
 
 class Square:
-    """Represents a square
+    """Represents a class  square
     Attributes:
         __size (int): size of a side of the square
     """
     def __init__(self, size):
-        """Initializes a square
+        """Initializes a class  square
         Args:
             size (int): size of a side of the square
         Returns: None
