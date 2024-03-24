99125e03912fed6fc7d85e65a2852cb5b089ff56
fniyonshuti <f.niyonshut1@alustudent.com>
2024-03-24 09:25:55 -0700

    Implemented script to get response body size from URL

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Send a GET request to the URL and measure the size of the response body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+#!/bin/bash 
+# take in a url and display the content 
+curl -sI "$1" | grep 'Content-Length'| cut -d " " -f2
