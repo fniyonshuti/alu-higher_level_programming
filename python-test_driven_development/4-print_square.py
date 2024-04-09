d630f5331baf332a9de65e495a1f388ea47250b8
fniyonshuti <f.niyonshut1@alustudent.com>
2024-04-09 13:04:06 -0700

    content changed

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""
-    Module containing function that prints a square
-"""
+''' Print square '''
 
 
 def print_square(size):
-    """ Function that prints a square using '#' based on `size`.
-
-    Args:
-        size (int): The size of one side of the square.
-    """
-
-    if type(size) is not int:
+    ''' Prints a square provided its length is a positive integer '''
+    if type(size) is not int or type(size) is float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    print(('#' * size + '\n') * size, end='')
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
